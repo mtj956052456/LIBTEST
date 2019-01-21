77f3c5c7269a6f06dd2007f79477826fabb5d976
Mengtengjiao <mtj5x5s@163.com>
2019-01-21 17:14:37 +0800

    Initial commit

---
+++ mylibrary/src/main/java/zhenqi/com/mylibrary/L.java
@@ -0,0 +1,12 @@
+package zhenqi.com.mylibrary;
+
+import android.util.Log;
+
+/**
+ * Created by lenovo on 2019/1/21.
+ */
+public class L {
+    public static void e() {
+        Log.e("CHEN", "test");
+    }
+}
