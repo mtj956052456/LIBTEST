4214727749ae88638db993b0ba99c4915da56534
Mengtengjiao <mtj5x5s@163.com>
2019-01-21 17:20:32 +0800

    dasda

--- app/src/main/java/zhenqi/com/libtest/MainActivity.java
+++ app/src/main/java/zhenqi/com/libtest/MainActivity.java
@@ -3,11 +3,14 @@ package zhenqi.com.libtest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import zhenqi.com.mylibrary.L;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        L.e();
     }
 }
